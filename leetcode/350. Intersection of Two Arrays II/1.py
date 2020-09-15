7c6ef81a046fd761a2a3d9d50364bf516833854b
luchev <luchevz@gmail.com>
2020-09-15 12:06:47 +0300

    Add solution

---
+++ leetcode/350. Intersection of Two Arrays II/1.py
@@ -0,0 +1,18 @@
+# Complexity (n = number of items in list 1, m = number of items in list 2)
+# Time complexity: O(n + m)
+# Space complexity: O(n)
+
+import collections
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        num1_counter = collections.Counter(nums1)
+        result = []
+        for num in nums2:
+            if num in num1_counter:
+                result.append(num)
+                num1_counter[num] -= 1
+                if num1_counter[num] == 0:
+                    del num1_counter[num]
+
+        return result
