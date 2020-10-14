375564a2784ee4801ebf19d417f124ed14b373ca
luchev <luchevz@gmail.com>
2020-10-14 12:40:44 +0300

    Add solution

---
+++ leetcode/213. House Robber II/2.py
@@ -0,0 +1,23 @@
+# Complexity (n = number of items in the list)
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        return max(self.rob_simple(nums[1:]), self.rob_simple(nums[:-1]))
+
+    def rob_simple(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        total = [nums[0], max(nums[0], nums[1])]
+        for i in range(2, len(nums)):
+            total.append(max(total[-1], total[-2] + nums[i]))
+            total = total[1:]
+        return total[-1]
