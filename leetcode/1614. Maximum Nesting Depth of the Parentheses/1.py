ffe031ffb0a607173a51544d0a778c99f5acdb1d
luchev <luchevz@gmail.com>
2020-12-02 12:00:34 +0200

    Add solution

---
+++ leetcode/1614. Maximum Nesting Depth of the Parentheses/1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        open_paren = 0
+        max_open = 0
+        for char in s:
+            if char == '(':
+                open_paren += 1
+                max_open = max(max_open, open_paren)
+            elif char == ')':
+                open_paren -= 1
+
+        return max_open
