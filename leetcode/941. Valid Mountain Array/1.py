3fb708eb15f52673a1d0b3072a327164f2de9f88
luchev <luchevz@gmail.com>
2020-12-10 10:46:05 +0200

    Add solution

---
+++ leetcode/941. Valid Mountain Array/1.py
@@ -0,0 +1,23 @@
+# Complexity (n = input array length)
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+from typing import List
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+        
+        for i in range(1, len(arr)):
+            if arr[i - 1] >= arr[i]:
+                break
+        
+        if i < 2:
+            return False
+        
+        for i in range(i, len(arr)):
+            if arr[i - 1] <= arr[i]:
+                return False
+        
+        return True
