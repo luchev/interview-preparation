8e6272dba55394135a1e8909cbc3da702c3d024b
luchev <luchevz@gmail.com>
2021-02-02 11:11:43 +0200

    Add solution

---
+++ leetcode/669. Trim a Binary Search Tree/1.go
@@ -0,0 +1,27 @@
+/**
+ * Complexity (n = number of nodes in the tree)
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil {
+		return root
+	}
+	if root.Val < low {
+		return trimBST(root.Right, low, high)
+	}
+	if high < root.Val {
+		return trimBST(root.Left, low, high)
+	}
+
+	root.Left = trimBST(root.Left, low, high)
+	root.Right = trimBST(root.Right, low, high)
+	return root
+}
