e189c341e711015392557813421a31f3578ad884
Ivan Luchev <luchevz@gmail.com>
2021-06-28 18:54:06 +0300

    Add solution

---
+++ leetcode/1047. Remove All Adjacent Duplicates In String/1.py
@@ -0,0 +1,13 @@
+# Complexity (n = input string length)
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if stack and stack[-1] == char:
+                stack.pop()
+            else:
+                stack.append(char)
+        return ''.join(stack)
